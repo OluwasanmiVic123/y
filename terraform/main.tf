fe972edc934571e7ab1b5ab7a4580b1c01c90b25
OluwasanmiVic123 <108752625+OluwasanmiVic123@users.noreply.github.com>
2023-03-22 17:43:07 +0000

    f

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "myekscluster"
+  cluster_name = "myeks"
 }
 
 resource "random_string" "suffix" {
@@ -30,7 +30,7 @@ module "vpc" {
   name = "kube-vpc"
 
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = slice(data.aws_availability_zones.available.names, 3)
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
